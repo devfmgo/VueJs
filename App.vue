afe822781acad95e0d1d2d1e0d0195ed8ed298fb
Muhamad <muhamafikri04@gmail.com>
2020-10-30 15:46:03 +0700

    custom button

--- App.vue
+++ App.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>Hello world</div>
+
+    <my-button
+    color="white"
+    background="darkslateblue"
+    disabled="true"
+    />
 </template>
 <script>
+import MyButton from "./MyButton.vue"
 export default {
     
+    components:{
+        MyButton
+    }
 }
 </script>
 
 <style scoped>
-   
+  
 </style>
---
+++ MyButton.vue
@@ -0,0 +1,36 @@
+<template>
+   <button :style="{background:background,color:color}">
+       Button
+   </button>
+</template>
+<script>
+export default {
+    props:{
+        color:{
+            type:String
+        },
+        background:{
+            type:String
+        },
+        disabled:{
+            type:Boolean
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button{
+       background: none;
+       color: black;
+       border:none;
+       border-radius: 5px;
+       padding: 10px 40px;
+       font-size: 16px;
+       cursor: pointer;
+       
+   }
+   button:hover{
+       filter: brightness(125%);
+   }
+</style>
