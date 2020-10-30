5f2dd6ece5b46e6fd0585e411274e95b0afda10f
Muhamad <muhamafikri04@gmail.com>
2020-10-30 09:40:11 +0700

    validation error

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
 import{createApp} from 'vue/dist/vue.esm-browser.js'
         const app = createApp({
             template:`
-            <button v-on:click="increment(6)">Increment</button>
+            <button @click="increment(5)">Increment</button>
             <p>{{count}}</p>
-            <div v-if="isEven()">
+            <div v-if="isEven(count)">
             Even 
             </div>
             <div v-else>
             Odd
             </div>
             <div v-for="number in numbers">
-            <div :class="getClass(number)">
+            <div :class="getClass(number)" :title="number">
             {{number}}
             </div>
             </div>
+            <input @input="input" :value="value"/>
+            <div v-if="error">{{error}}</div>
             `,
             computed:{
                 evenList(){
                     return this.numbers.filter(this.isEven)
+                },
+                error(){
+                    if (this.value.length < 7) {
+                        return "Too short"
+                    }
                 }
             },
             data(){
                 return{
                     count:0,
-                    numbers:[1,2,3,4,5,6,7,8,9,10]
+                    numbers:[1,2,3,4,5,6,7,8,9,10],
+                    value:'User',
                 }
             },
             methods:{
+                input($evt){
+               this.value = $evt.target.value
+                },
                 getClass(number){
                     if (this.isEven(number)) {
                         return "red"
